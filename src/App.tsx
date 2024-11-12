2b7242ac438439218fd170ee4537c2c0fb935340
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2024-11-12 19:47:05 +0700

    chore: remove redundance comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// // import Home from "./pages/Home";
-// // import Login from "./pages/Login";
-// import Register from "./components/Register";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* <Route path="/" element={<Home />} />
-//         <Route path="/login" element={<Login />} /> */}
-//         <Route path="/register" element={<Register />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,3 @@
-// // src/pages/Home.tsx
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// const Home: React.FC = () => {
-//   return (
-//     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-//       <h1 className="text-3xl font-bold text-gray-800 mb-4">
-//         Welcome to the User Registration System
-//       </h1>
-//       <p className="text-lg text-gray-600 mb-6">
-//         Please log in or register to access your account.
-//       </p>
-//       <div className="space-x-4">
-//         <Link to="/login">
-//           <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
-//             Login
-//           </button>
-//         </Link>
-//         <Link to="/register">
-//           <button className="px-4 py-2 bg-green-500 text-white font-semibold rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300">
-//             Register
-//           </button>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
 // export default Home;
 
 // src/pages/Home.tsx
